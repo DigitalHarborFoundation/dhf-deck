301ecd766bd62ba4d443e296c08a75d210d53f04
jonathanprozzi <jonathanprozzi@gmail.com>
2018-04-27 00:39:55 -0400

    added .prettierrc settings so that my trailing commas stop deleting ^-^

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     name: `Digital Harbor Foundation`,
-    title: `Our Culture`
+    title: `Our Culture`,
     // date: `April 24, 2018`
   },
   plugins: [
@@ -12,29 +12,30 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-smartypants`]
-      }
+        plugins: [`gatsby-remark-smartypants`],
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `slides`,
-        path: `${__dirname}/src/pages/`
-      }
+        path: `${__dirname}/src/pages/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/src/images/`
-      }
+        path: `${__dirname}/src/images/`,
+      },
     },
     {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
         postCssPlugins: [],
-        precision: 8
-      }
-    }
-  ]
+        precision: 8,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 };
